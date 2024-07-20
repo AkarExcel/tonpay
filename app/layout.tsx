2423249b1f3291c8b02aaee5eae4b9676628de5a
akarexcel <therealakarexcel@gmail.com>
2024-07-19 21:48:09 -0500

    Home page v1 completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navmenu from '../components/Navmenu'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background`}>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={`${inter.className} bg-background text-white`}>
+        {children}
+      <Navmenu />
+      </body>
     </html>
   );
 }
